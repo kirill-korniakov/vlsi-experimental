6cd9fede398bbad4eb3bd7b07b0737b6c86c773a
Kirill Kornyakov <no-email@email.com>
2009-05-11 20:37:53 +0000

    last revision of our implementation of APlace algorithm

--- AnalyticalPlacer/include/petsc-2.3.3-p6/bmake/win32_ms/petscconfiginfo.h
+++
@@ -1,2 +0,0 @@
-static const char *petscconfigureruntime = "Mon Nov 19 10:35:53 2007";
-static const char *petscconfigureoptions = "--with-clanguage=C++ --with-cc=\"win32fe cl --nodetect\" --with-cxx=\"win32fe cl --nodetect\" --with-fc=0 --with-mpi-include=\"/cygdrive/c/program files/mpich2/include\" --with-mpi-lib=\"/cygdrive/c/program files/mpich2/lib/mpi.lib\" --useThreads=0 --with-shared=0";
