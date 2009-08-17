e97e8438e601ced7ffc3deb6d86fb51b8c6278a9
Alexander Belyakov <no-email@email.com>
2009-08-17 07:41:35 +0000

    Added FastRouter project for OS Windows and archive with original FastRouter's code.

---
+++ Roters_win/FastRouter/RSMT.h
@@ -0,0 +1,12 @@
+#ifndef _RSMT_H_
+#define _RSMT_H_
+
+extern int ** V_table;
+extern int ** H_table;
+
+extern void gen_brk_RSMT(Bool congestionDriven, Bool reRoute, Bool genTree, Bool newType, Bool noADJ);
+extern void fluteNormal(int netID, int d, DTYPE x[], DTYPE y[], int acc, float coeffV, Tree *t);
+extern void fluteCongest(int netID, int d, DTYPE x[], DTYPE y[], int acc, float coeffV, Tree *t);
+extern void copyStTree(int ind, Tree rsmt);
+
+#endif /* _RSMT_H_ */
