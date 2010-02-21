f5efd00e2c2e2c2f6aaec53941591b117720742d
Andrey Kamaev <no-email@email.com>
2010-02-21 00:08:37 +0000

    Scripts update.

--- itlPlace/bin/run_iwls_gslib_one_iteration_buf.bat
+++ itlPlace/bin/run_iwls_gslib_one_iteration_buf.bat
@@ -1,6 +1,8 @@
 @echo off
 for /f %%b in (.\IWLS05\original_list.txt) do (
 itlPlacerelease.exe Config_for_night_experiments.cfg --params.def=.\IWLS05\%%b.def --DesignFlow.nMacroIterations=1 --DesignFlow.Buffering=true --NetWeighting.method=APlace --params.techname=IWLS
-copy /Y Replicated_Config_for_night_experiments.cfg .\cfg\%date:~-4,4%%date:~-7,2%%date:~-10,2%%time:~-11,2%%time:~-8,2%%time:~-5,2%%time:~-2,2%_oneb_%%b.cfg >nul
+set hh=%time:~0,2%
+if "%time:~0,1%"==" " set hh=0%hh:~1,1%
+copy /Y Replicated_Config_for_night_experiments.cfg .\cfg\%date:~-4,4%%date:~-7,2%%date:~-10,2%%hh%%time:~-8,2%%time:~-5,2%%time:~-2,2%_oneb_%%b.cfg >nul
 echo %date% %time% %%b.def completed
 )
--- itlPlace/bin/run_iwls_nangate_alt_iteration_buf.bat
+++ itlPlace/bin/run_iwls_nangate_alt_iteration_buf.bat
@@ -1,6 +1,8 @@
 @echo off
 for /f %%b in (.\IWLS05\original_list.txt) do (
 itlPlacerelease.exe Config_for_night_experiments.cfg --params.def=.\IWLS05\%%b.def --DesignFlow.nMacroIterations=9 --DesignFlow.Buffering=true --NetWeighting.method=SensitivityGuidedNetWeighting --params.techname=NangateMapped
-copy /Y Replicated_Config_for_night_experiments.cfg .\cfg\%date:~-4,4%%date:~-7,2%%date:~-10,2%%time:~-11,2%%time:~-8,2%%time:~-5,2%%time:~-2,2%_ngaltb_%%b.cfg >nul
+set hh=%time:~0,2%
+if "%time:~0,1%"==" " set hh=0%hh:~1,1%
+copy /Y Replicated_Config_for_night_experiments.cfg .\cfg\%date:~-4,4%%date:~-7,2%%date:~-10,2%%hh%%time:~-8,2%%time:~-5,2%%time:~-2,2%_ngaltb_%%b.cfg >nul
 echo %date% %time% %%b.def completed
 )
