87f6ac6cd4c5d2d480670823584f267db34c7109
Ilya Levedev <no-email@email.com>
2010-05-31 15:09:07 +0000

    #63 довел до конца работу над скриптом

--- TestingFramework/CoreScripts/LogParser.py
+++ TestingFramework/CoreScripts/LogParser.py
@@ -49,3 +49,48 @@ class LogParser:
             if lineIdx == len(lines):
                 print('Stage ' + stageTag + ' not found')
                 return NOT_FOUND
+
+    def GetStageTag(self):
+        log = open(self.logName, 'r')
+        lines = log.readlines()
+        log.close()
+        lineIdx = 0
+        for line in lines:
+            if line.find(self.parameters.tableHeader) != -1:
+
+                metrics = lines[lineIdx+1].split()
+                colIdx = 0
+                metricTag = 'Tag';
+                for metric in metrics:
+                    if metrics[colIdx].find(metricTag) == 0:
+                        break
+                    else:
+                        colIdx = colIdx + 1
+                if colIdx == len(metrics):
+                    print('Tag ' + metricTag + ' not found')
+                    return NOT_FOUND
+                
+                lineIdx = lineIdx + 3
+                tagId = 0
+                
+                columCount = 0;
+                i = lineIdx
+                while lines[i].find(self.parameters.borderPattern) == -1:
+                    columCount = columCount + 1
+                    i = i + 1
+                
+                tagList = ['' for i in range(columCount)]
+                while lines[lineIdx].find(self.parameters.borderPattern) == -1:
+                    ll = lines[lineIdx].split()
+                    tagList[tagId] = ll[colIdx]
+                    tagId = tagId + 1
+                    lineIdx = lineIdx + 1
+                
+                res = [tagId, tagList]
+                return res
+            else:
+                lineIdx = lineIdx + 1
+        if lineIdx == len(lines):
+            print(self.parameters.tableHeader + ' not found in log file')
+            return NOT_FOUND
+        
--- TestingFramework/Experiment_New_Buffering.py
+++ TestingFramework/Experiment_New_Buffering.py
@@ -7,20 +7,34 @@ from Parameters import *
 class Experiment_New_Buffering(BaseExperiment):
     def CreateEmptyTable(self, reportTable):
         po = open(reportTable, 'w')
-        po.write('Benchmark;bbHPWL;abHPWL;bbTWL;abTWL;bbTNS;abTNS;bbWNS;abWNS\n')
+        po.write('Benchmark;bbHPWL;abHPWL;bbTWL;abTWL;bbTNS;abTNS;bbWNS;abWNS;TIME;Cells;%HPWL;%TWL;%TNS;%WNS;%AddBuf\n')
         po.close()
 
     def ParseLogAndFillTable(self, logName, benchmark, reportTable):
         #get metrics
+        metricTagList = ['StageName', 'Tag', 'Time(sec)', 'Cells', 'HPWL(nm)', 'TWL(nm)', 'TNS(ns)', 'WNS(ns)']
         parser = LogParser(logName)
-        bbHPWL = parser.GetFromPFST('INIT', 4)
-        abHPWL = parser.GetFromPFST('LEGB', 4)
-        bbTWL = parser.GetFromPFST('INIT', 5)
-        abTWL = parser.GetFromPFST('LEGB', 5)
-        bbTNS = parser.GetFromPFST('INIT', 6)
-        abTNS = parser.GetFromPFST('LEGB', 6)
-        bbWNS = parser.GetFromPFST('INIT', 7)
-        abWNS = parser.GetFromPFST('LEGB', 7)
+        tagCount = parser.GetStageTag()[0]
+        print('last tag = ' + str(tagCount))
+        tagList = parser.GetStageTag()[1]        
+        for i in tagList:
+            print('tag = ' + i)
+        bbCells = parser.GetFromPFST(tagList[0], metricTagList[3])
+        abCells = parser.GetFromPFST(tagList[-1], metricTagList[3])
+        abTime = parser.GetFromPFST(tagList[-1], metricTagList[2])
+        bbHPWL = parser.GetFromPFST(tagList[0], metricTagList[4])
+        abHPWL = parser.GetFromPFST(tagList[-1], metricTagList[4])
+        bbTWL = parser.GetFromPFST(tagList[0], metricTagList[5])
+        abTWL = parser.GetFromPFST(tagList[-1], metricTagList[5])
+        bbTNS = parser.GetFromPFST(tagList[0], metricTagList[6])
+        abTNS = parser.GetFromPFST(tagList[-1], metricTagList[6])
+        bbWNS = parser.GetFromPFST(tagList[0], metricTagList[7])
+        abWNS = parser.GetFromPFST(tagList[-1], metricTagList[7])        
+        pHPWL = (1 - float(abHPWL.replace(',', '.')) / float(bbHPWL.replace(',', '.'))) * 100
+        pTWL = (1 - float(abTWL.replace(',', '.')) / float(bbTWL.replace(',', '.'))) * 100 
+        pTNS = (1 - float(abTNS.replace(',', '.')) / float(bbTNS.replace(',', '.'))) * 100
+        pWNS = (1 - float(abWNS.replace(',', '.')) / float(bbWNS.replace(',', '.'))) * 100
+        AddBuf = float(abCells) - float(bbCells)
 
         #print metrics
         printStr = benchmark + ';'
@@ -32,6 +46,14 @@ class Experiment_New_Buffering(BaseExperiment):
         printStr += str(abTNS) + ';'
         printStr += str(bbWNS) + ';'
         printStr += str(abWNS) + ';'
+        printStr += str(abTime) + ';'
+        printStr += str(bbCells) + ';'
+        printStr += str(pHPWL) + ';'
+        printStr += str(pTWL) + ';'
+        printStr += str(pTNS) + ';'
+        printStr += str(pWNS) + ';'
+        printStr += str(AddBuf) + ';'
+        
         printStr += '\n'
         print(printStr)
 
@@ -42,9 +64,9 @@ class Experiment_New_Buffering(BaseExperiment):
 
 def test():
     testRunner = TestRunner()
-    e = Experiment_New_Buffering('IWLS05 new_buffering experiment (GBL)', 'New_buffering.cfg', 'IWLS05_GP.list')
+    e = Experiment_New_Buffering('IWLS05 new_buffering experiment (GBL)', 'New_buffering.cfg', 'IWLS_GP_r1511.list')
     testRunner.parameters.experiments.append(e)
 
     testRunner.Run()
 
-#test()
+test()
