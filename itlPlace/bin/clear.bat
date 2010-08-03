aa0be2acd4b7cc011449f3a94c4deee61356b133
Kirill Kornyakov <no-email@email.com>
2010-08-03 13:57:49 +0000

    удалил кучу мусора из bin

--- itlPlace/bin/clear.bat
+++ itlPlace/bin/clear.bat
@@ -1,4 +1,3 @@
-del .\ClusteringInformation\*.ci
 del .\ExportDEF\*.def
 del .\log\*.log
 del .\log\*.htm
@@ -6,7 +5,6 @@ del .\log\*.html
 del .\pix\*.jpg
 del .\pix\milestones\*.jpg
 del .\video\*.avi
-del .\cfg\*.cfg
 del .\*.nwts
 del .\Replicated_*.cfg
 del .\*Warning.log
