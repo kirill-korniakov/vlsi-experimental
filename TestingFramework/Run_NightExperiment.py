cafa022b2445de763911a7b69b81689a7fbbf2aa
Alexander Belyakov <no-email@email.com>
2010-08-23 20:05:38 +0000

    Mistake was fixed

--- TestingFramework/Run_NightExperiment.py
+++ TestingFramework/Run_NightExperiment.py
@@ -1,3 +1,6 @@
+import BaseChecker
+from BaseChecker import *
+
 import HPWLPlacementChecker
 from HPWLPlacementChecker import *
 
@@ -6,7 +9,7 @@ from LRPlacementChecker import *
 
 def NightExperiment():
     exp_HPWL_ISPD04 = BaseChecker('ISPD04 HPWL experiment', 'hpwl_ispd04.cfg', 'ISPD04.list', "MasterLogs/HPWL/ISPD", ['HPWL'], ['DP'])
-    exp_HPWL_IWLS05 = BasePlacementChecker('IWLS05 HPWL experiment', 'hpwl_iwls05.cfg', 'IWLS05.list', "MasterLogs/HPWL/IWLS", ['HPWL'], ['DP'])
+    exp_HPWL_IWLS05 = BaseChecker('IWLS05 HPWL experiment', 'hpwl_iwls05.cfg', 'IWLS05.list', "MasterLogs/HPWL/IWLS", ['HPWL'], ['DP'])
     exp_LR_IWLS05GP = LRPlacementChecker('IWLS05GP LR experiment', 'LR.cfg', 'IWLS_GP_r1511.list', "MasterLogs/LR")
 
     testRunner = TestRunner()
