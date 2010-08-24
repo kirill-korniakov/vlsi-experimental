e648d037ccf0115e5ef4226156123e4d83ff22d0
Ilya Levedev <no-email@email.com>
2010-08-24 18:56:01 +0000

    добавил мастер логи новой буферизации, исправил падение скрипта с новой буферизацией

--- TestingFramework/CoreScripts/LogParser.py
+++ TestingFramework/CoreScripts/LogParser.py
@@ -55,6 +55,10 @@ class LogParser:
         lines = log.readlines()
         log.close()
         lineIdx = 0
+        linesCount = 0
+        for line in lines:
+            linesCount = linesCount + 1
+            
         for line in lines:
             if line.find(self.parameters.tableHeader) != -1:
 
@@ -75,12 +79,12 @@ class LogParser:
                 
                 columCount = 0;
                 i = lineIdx
-                while ((lines[i].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1)):
+                while ((lines[i].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1) and (i <= linesCount)):
                     columCount = columCount + 1
                     i = i + 1
                 
                 tagList = ['' for i in range(columCount)]
-                while ((lines[lineIdx].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1)):
+                while ((lines[lineIdx].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1) and (i <= linesCount)):
                     ll = lines[lineIdx].split()
                     tagList[tagId] = ll[colIdx]
                     tagId = tagId + 1
