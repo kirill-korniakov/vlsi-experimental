1edeb4189cd0d5bb31cdc02d9b24ceec1b23e4bd
Alexander Belyakov <no-email@email.com>
2010-08-28 18:28:11 +0000

    Syntax errors fixed

--- TestingFramework/Run_NightExperiment.py
+++ TestingFramework/Run_NightExperiment.py
@@ -1,16 +1,10 @@
 import BaseChecker
 from BaseChecker import *
 
-import HPWLPlacementChecker
-from HPWLPlacementChecker import *
-
-import LRPlacementChecker
-from LRPlacementChecker import *
-
 def NightExperiment():
     exp_HPWL_ISPD04 = BaseChecker('ISPD04 HPWL experiment', 'hpwl_ispd04.cfg', 'ISPD04.list', "MasterLogs/HPWL/ISPD", ['HPWL'], ['DP'])
     exp_HPWL_IWLS05 = BaseChecker('IWLS05 HPWL experiment', 'hpwl_iwls05.cfg', 'IWLS05.list', "MasterLogs/HPWL/IWLS", ['HPWL'], ['DP'])
-    exp_LR_IWLS05GP = LRPlacementChecker('IWLS05GP LR experiment', 'LR.cfg', 'IWLS_GP_r1511/IWLS_GP.list', "MasterLogs/LR")
+    exp_LR_IWLS05GP = BaseChecker('IWLS05GP LR experiment', 'LR.cfg', 'IWLS_GP_r1511/IWLS_GP.list', "MasterLogs/LR")
 
     buffMetrics = ['HPWL', 'TWL', 'TNS', 'WNS']
     buffStages  = ['INIT', 'NBUF']
