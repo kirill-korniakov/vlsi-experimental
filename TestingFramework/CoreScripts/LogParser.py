3bd7e234ceb7e9d9ee7554687dcf7cdd14527e79
Alexander Belyakov <no-email@email.com>
2010-09-12 15:41:09 +0000

    Changed searching of stages

--- TestingFramework/CoreScripts/LogParser.py
+++ TestingFramework/CoreScripts/LogParser.py
@@ -42,7 +42,7 @@ class LogParser:
         #parse stages
         lineIdx = lineIdx + 3
         while lines[lineIdx].find(self.parameters.borderPattern) == -1:
-            if lines[lineIdx].find(stageTag) != -1:
+            if lines[lineIdx].find(stageTag + ' ') != -1:
                 ll = lines[lineIdx].split()
                 return ll[colIdx]
             lineIdx = lineIdx + 1
@@ -58,7 +58,7 @@ class LogParser:
         linesCount = 0
         for line in lines:
             linesCount = linesCount + 1
-            
+
         for line in lines:
             if line.find(self.parameters.tableHeader) != -1:
 
@@ -73,23 +73,23 @@ class LogParser:
                 if colIdx == len(metrics):
                     print('Tag ' + metricTag + ' not found')
                     return NOT_FOUND
-                
+
                 lineIdx = lineIdx + 3
                 tagId = 0
-                
+
                 columCount = 0;
                 i = lineIdx
                 while ((lines[i].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1) and (i <= linesCount)):
                     columCount = columCount + 1
                     i = i + 1
-                
+
                 tagList = ['' for i in range(columCount)]
                 while ((lines[lineIdx].find(self.parameters.borderPattern) == -1) and (lines[i].find("Thats All") == -1) and (i <= linesCount)):
                     ll = lines[lineIdx].split()
                     tagList[tagId] = ll[colIdx]
                     tagId = tagId + 1
                     lineIdx = lineIdx + 1
-                
+
                 res = [tagId, tagList]
                 return res
             else:
@@ -97,4 +97,4 @@ class LogParser:
         if lineIdx == len(lines):
             print(self.parameters.tableHeader + ' not found in log file')
             return NOT_FOUND
-        
+
--- TestingFramework/Experiment_SGNW.py
+++ TestingFramework/Experiment_SGNW.py
@@ -8,22 +8,15 @@ from Parameters import *
 class Experiment_SGNW(BaseExperiment):
     def __init__(self):
         _metrics = ['HPWL', 'TNS', 'WNS']
-        _stages  = ['GP1', 'LEG1', 'GP9', 'LEG9']
+        _stages  = ['GP1', 'LEG1', 'GP', 'LEG']
         BaseExperiment.__init__(self, 'Weighting (SGNW) experiment',\
             'Sensitivity_guided_weighting.cfg', 'IWLS05.list',\
             _metrics, _stages)
 
 def test():
     testRunner = TestRunner()
-
-    #eISPD = Experiment_HPWL('ISPD04 experiment', 'hpwl_ispd04.cfg', 'ISPD04.list')
-    #eIWLS = Experiment_HPWL('IWLS05 HPWL experiment', 'hpwl_iwls05.cfg', 'IWLS05_fast.list', ['HPWL'], ['DP'])
-    eIWLS = BaseExperiment('IWLS05 HPWL experiment', 'hpwl_iwls05.cfg',\
-        'IWLS05_fast.list', ['HPWL'], ['DP'])
-
-    testRunner.parameters.experiments.append(eIWLS)
-    #testRunner.parameters.experiments.append(eISPD)
-
+    eSGNW      = Experiment_SGNW()
+    testRunner.Append(eSGNW)
     testRunner.Run()
 
 #test()
