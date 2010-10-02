7c9a1a8a62ba9e6b7975f3d8e0f2410afbe539f9
Alexander Belyakov <no-email@email.com>
2010-10-02 10:20:41 +0000

    LR refactoring

--- TestingFramework/Experiment_LR.py
+++ TestingFramework/Experiment_LR.py
@@ -12,64 +12,44 @@ class Experiment_LR(BaseExperiment):
             'IWLS_GP_r1511/IWLS_GP.list', _metrics, _stages)
 
     def CreateEmptyTable(self, reportTable):
-        po = open(reportTable, 'w')
-        #NOTE: do not format the string below, because it harms table formatting
-        po.write('Benchmark;initHPWL;lrHPWL;dpHPWL;\
-initTWL;lrTWL;dpTWL;\
-initTNS;lrTNS;dpTNS;\
-initWNS;lrWNS;dpWNS;;\
-lrHPWL%;dpHPWL%;\
-lrTWL%;dpTWL%;\
-lrTNS%;dpTNS%;\
-lrWNS%;dpWNS%\n')
-        po.close()
+        cols = ['Benchmark', END_OF_COLUMN, 'initHPWL', END_OF_COLUMN, 'lrHPWL', END_OF_COLUMN, 'dpHPWL', END_OF_COLUMN,\
+        'initTWL', END_OF_COLUMN, 'lrTWL', END_OF_COLUMN, 'dpTWL', END_OF_COLUMN,\
+        'initTNS', END_OF_COLUMN, 'lrTNS', END_OF_COLUMN, 'dpTNS', END_OF_COLUMN,\
+        'initWNS', END_OF_COLUMN, 'lrWNS', END_OF_COLUMN, 'dpWNS', END_OF_COLUMN, END_OF_COLUMN,\
+        'lrHPWL%', END_OF_COLUMN, 'dpHPWL%', END_OF_COLUMN, 'lrTWL%', END_OF_COLUMN, 'dpTWL%', END_OF_COLUMN,\
+        'lrTNS%', END_OF_COLUMN, 'dpTNS%', END_OF_COLUMN, 'lrWNS%', END_OF_COLUMN, 'dpWNS%']
 
-    def ParseLogAndFillTable(self, logName, benchmark, reportTable):
-        #define metrics
-        metrics = ['HPWL', 'TWL', 'TNS', 'WNS']
-        stages = ['INIT', 'LEG']#, 'DP']
+        WriteStringToFile(cols, reportTable)
 
-        #read numbers
-        parser = LogParser(logName)
-        table = [[0 for col in range(len(metrics))] for row in range(len(stages))]
-        for col in range(len(metrics)):
-            for row in range(len(stages)):
-                value = str(parser.GetFromPFST(stages[row], metrics[col]))
-                table[row][col] = float(value.replace(',', '.'))
+    def AddStringToTable(self, values, benchmark, reportTable):
+        cols = [benchmark, END_OF_COLUMN]
 
         #print absolute values
-        printStr = benchmark + ';'
         for col in range(len(metrics)):
             for row in range(len(stages)):
-                printStr += str(table[row][col]) + ';'
-        printStr = printStr + ';'
+                cols.append(str(values[row][col]))
+                cols.append(END_OF_COLUMN)
+
+        cols.append(END_OF_COLUMN)
 
         #print percents
         for col in range(len(metrics)):
             for row in range(1, len(stages)):
-                if table[0][col] != 0.0:
-                    percent = 100.0 * (table[row][col]/table[0][col] - 1.0)
+                if values[0][col] != 0.0:
+                    percent = 100.0 * (values[row][col]/values[0][col] - 1.0)
+
                 else:
                     percent = -1.0
-                printStr += "%.2f;" % percent
-        print(printStr)
 
-        #write metrics to the file
-        table = open(reportTable, 'a')
-        table.write(printStr.replace('.', ','))
-        table.close()
+                printStr = "%.2f;" % percent
+                cols.append(printStr)
+
+        WriteStringToFile(cols, reportTable)
 
 def test():
+    e = Experiment_LR()
     testRunner = TestRunner()
-    metrics = ['HPWL', 'TWL', 'TNS', 'WNS']
-    stages  = ['INIT', 'LEG']
-
-    cmdArgs = ''#--LR.GlobalPlacement.LagrangianRelaxation.alphaTWL=1.0e-5'
-    e = Experiment_LR('IWLS05 LR after weighting experiment', 'LR.cfg',\
-        'IWLS_GP_r1511/IWLS_GP.list', metrics, stages, cmdArgs)
-    #e = BaseExperiment('IWLS05 LR after weighting experiment', 'LR.cfg', 'IWLS_GP_r1511/IWLS_GP.list', metrics, stages, cmdArgs)
     testRunner.parameters.experiments.append(e)
-
     testRunner.Run()
 
 #test()
--- TestingFramework/Run_NightExperiment_fast.py
+++ TestingFramework/Run_NightExperiment_fast.py
@@ -11,10 +11,10 @@ def RunFastExperiments():
     fast_lists = {\
 #        "IWLS05 HPWL experiment":"IWLS05_fast.list",\
 #        "HPWL_ISPD":"ISPD04_fast.list",\
-#        "IWLS05GP LR experiment":"IWLS_GP_r1511/IWLS_GP_fast.list",\
+        "IWLS05GP LR experiment":"IWLS_GP_r1511/IWLS_GP_fast.list",\
 #        "IWLS05 new_buffering experiment (GBL)":"IWLS05_fast.list",\
-        "HippocrateDP experiment":"IWLS_GP_Hippocrate.list",\
-        "Weighting (SGNW) experiment":"IWLS05_fast.list",\
+#        "HippocrateDP experiment":"IWLS_GP_Hippocrate.list",\
+#        "Weighting (SGNW) experiment":"IWLS05_fast.list",\
 #        "APlace weighting experiment":"IWLS05_fast.list",\
         }
 
