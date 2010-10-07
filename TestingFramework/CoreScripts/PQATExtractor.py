b72ba7cc20744ef50c7a27c7a6cfcefc940e2695
Alexander Belyakov <no-email@email.com>
2010-10-07 19:12:45 +0000

    Added new script for parsing PQAT from *.log files in specified folder.

---
+++ TestingFramework/CoreScripts/PQATExtractor.py
@@ -0,0 +1,19 @@
+import LogParser
+from LogParser import *
+
+def ParsePQAT(logFolder = ''):
+    metrics = ['HPWL', 'TNS', 'WNS']
+
+    if (logFolder == ''):
+      logFolder = os.getcwd()
+
+    for log in os.listdir(logFolder):
+      if (os.path.isfile(os.path.join(logFolder, log)) and ('.log' == os.path.splitext(log)[-1])):
+        parser = LogParser(logFolder + '/' + log)
+        parser.ParsePQATAndPrintTable(metrics)
+
+def Run():
+    logFolder = '../Reports/LR.cfg_2010-10-07_22-37-59'
+    ParsePQAT(logFolder)
+
+Run()
