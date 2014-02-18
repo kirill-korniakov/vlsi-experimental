9ee9a4394309b3505ed41396339d8a819a7e0038
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2014-02-18 23:24:31 +0400

    Added disabled test for SGNW testing

--- TestingFramework/AcceptanceTest/TestRegression.py
+++ TestingFramework/AcceptanceTest/TestRegression.py
@@ -71,7 +71,7 @@ class TestRegression:
         for benchmark in benchmarks:
             yield self.test_helper.run, self.logger, experiment, benchmark, referenceLogFolder
 
-    # APlace Weighting crashes
+    # Both weighting approaches crash on clustering
     @nottest
     def test_aplace_weighting(self):
         benchmark_list = "regression/APlace_Weighting.list"
@@ -84,10 +84,15 @@ class TestRegression:
         for benchmark in benchmarks:
             yield self.test_helper.run, self.logger, experiment, benchmark, referenceLogFolder
 
-    # TODO: enable tests below
-    ## Sensitivity-Guided Weighting, "IWLS05.list" -- crash
-    # exp_W = Experiment_Weighting()
-    # chk_SGW = Checker(exp_W, referenceLogs + r"/Weighting/SensitivityGuided")
+    @nottest
+    def test_sgnw_weighting(self):
+        benchmark_list = "regression/SGNW_Weighting.list"
+        experiment = Experiment_Weighting(self.logger)
+        referenceLogFolder = "/Weighting/SensitivityGuided"
+
+        benchmarks = self.test_helper.expand_benchmark_list(benchmark_list)
+        for benchmark in benchmarks:
+            yield self.test_helper.run, self.logger, experiment, benchmark, referenceLogFolder
 
 
 if __name__ == "__main__":
